b9f76ddf2ad98eefadea91dd3d1c189ec9f68bf8
Tsm8542 <shaikhabid932@gmail.com>
2023-10-03 21:01:32 +0530

    03-10-2023 09:01 PM

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
+function create_option(option_value,inner_html,start,limit){
+    let x ="";
+    for(let i=start;i<limit+1;i++){
+        let a = '<option value="'+String(option_value+i)+'">'+inner_html+i+'</option>';
+        x += a;
+    }
+    return x;
+}
+function create_pr(std,limit){
+    let x="";
+    let m = 1;
+    let n=30;
+    for(let i=1;i<limit+1;i++){
+        let mn = m+'-'+n;
+        let a = '<option value="std_'+std+'_pr_'+mn+'">'+mn+'</option>';
+        x+=a;
+        m+=30;n+=30;
+    }
+    x+='<option value="all'+std+'">All</option>';
+    return x;
+}
 function x(){
     var s = document.querySelector("#standard").value;
     var t = document.querySelector("#page_range");
     switch(s){
         case 'std11':
             tg("true");
-            t.innerHTML = '<option value="std_11_pr_1-30">1-30</option><option value="std_11_pr_31-60">31-60</option><option value="std_11_pr_61-90">61-90</option><option value="std_11_pr_91-120">91-120</option><option value="std_11_pr_121-150">121-150</option><option value="all11">All</option>'
+            t.innerHTML = create_pr(11,5);
             break;
         case 'std12':
             tg("true");
-            t.innerHTML = '<option value="std_12_pr_1-30">1-30</option><option value="std_12_pr_31-60">31-60</option><option value="std_12_pr_61-90">61-90</option><option value="std_12_pr_91-120">91-120</option><option value="all12">All</option>'
+            t.innerHTML = create_pr(12,4)
             break;
         case 'download':
-            t.innerHTML="";
             tg("false");
             break;
         default:
             tg("true");
-            t.innerHTML = '<option value="std_11_pr_1-30">1-30</option><option value="std_11_pr_31-60">31-60</option><option value="std_11_pr_61-90">61-90</option><option value="std_11_pr_91-120">91-120</option><option value="std_11_pr_121-150">121-150</option><option value="all11">All</option>'
+            t.innerHTML = create_pr(11,5);
             break;
     }
     z();
@@ -25,45 +45,31 @@ function x(){
 function z(){
     var n = document.querySelector("#page_range").value;
     var y = document.querySelector("#frame");
-    switch(n){
-        case 'std_11_pr_1-30':
-            y.src = "Arabic Std 11/pr_1-30.html"
+    let lang = "Arabic";
+    let m="";
+    for(let i=0;i<n.length;i++){
+        if(n[i]=='p'){
+            for(let z=i;z<n.length;z++){
+                m+=n[z];
+            }
             break;
-        case 'std_11_pr_31-60':
-            y.src = "Arabic Std 11/pr_31-60.html"
+        }
+        else if(n[i]=='a'){
+            m="main";
             break;
-        case 'std_11_pr_61-90':
-            y.src = "Arabic Std 11/pr_61-90.html"
-            break;    
-        case 'std_11_pr_91-120':
-            y.src = "Arabic Std 11/pr_91-120.html"
-            break;
-        case 'std_11_pr_121-150':
-            y.src = "Arabic Std 11/pr_121-150.html"
-            break;
-        case 'all11':
-            y.src = "Arabic Std 11/main.html"
-            break;
-        case 'std_12_pr_1-30':
-            y.src = "Arabic Std 12/pr_1-30.html"
-            break;
-        case 'std_12_pr_31-60':
-            y.src = "Arabic Std 12/pr_31-60.html"
-            break;
-        case 'std_12_pr_61-90':
-            y.src = "Arabic Std 12/pr_61-90.html"
-            break;
-        case 'std_12_pr_91-120':
-            y.src = "Arabic Std 12/pr_91-120.html"
-            break;
-        case 'all12':
-            y.src = "Arabic Std 12/main.html"
-            break;
+        }
     }
+    let x = document.querySelector("#standard").value;
+    let std ="";
+    for(let i=3;i<x.length;i++){
+        std += x[i];
+    }
+    y.src = lang+' Std '+std+'/'+m+'.html';
 }
 
 function tg(v){
     if(v=="true"){
+        document.querySelector("#standard").style.display="inline-block";
         document.querySelector("#page_range").style.display="inline-block";
         document.querySelector("#frame").style.display="block"
     }
